741e7f9ac4d324f2f876464e5ffe5bead024eab6
Nirpa Dani <g3.nirpa@gmail.com>
2022-04-18 20:56:35 +1000

    adding files to github

---
+++ src/components/footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  height: auto;
+  min-height: 70px;
+  padding: 15px;
+  text-align: center;
+`;
+
+const StyledCredit = styled.div`
+  color: var(--light-slate);
+  font-family: var(--font-mono);
+  font-size: var(--fz-xxs);
+  line-height: 1;
+
+  a {
+    padding: 10px;
+  }
+
+  .github-stats {
+    margin-top: 10px;
+
+    & > span {
+      display: inline-flex;
+      align-items: center;
+      margin: 0 7px;
+    }
+    svg {
+      display: inline-block;
+      margin-right: 5px;
+      width: 14px;
+      height: 14px;
+    }
+  }
+`;
+
+const Footer = () => (
+  <StyledFooter>
+    <StyledCredit tabindex="-1">
+      <a href="https://github.com/iowaa">
+        <div>Designed &amp; Built by Nirpa Dani</div>
+      </a>
+    </StyledCredit>
+  </StyledFooter>
+);
+
+export default Footer;
